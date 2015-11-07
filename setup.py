019c6a8de9b07cd619b1d2a98670844edbc42744
Steven Osborn <osborn.steven@gmail.com>
2015-11-07 00:01:25 -0800

    setup

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ Flask-rt
 
 Realtime notification system for model state changes via pusher app
 """
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name='Flask-rt',
-    version='1.0',
+    version='1.1',
     url='https://github.com/Odava/flask-rt',
     license='Proprietary',
     author='Steven Osborn',
@@ -17,7 +16,7 @@ setup(
     description='Notifies clients of model state changes via pusher app ',
     long_description=__doc__,
     zip_safe=False,
-    packages=['flask_rt'],
+    packages=find_packages(),
     install_requires=[
         'Flask',
         'pusher',
